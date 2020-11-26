7d14db812186a6b64f30ba6d7a3100af060cda32
Joonas <joonas.ervald@gmail.com>
2020-11-26 12:13:30 +0200

    Init

---
+++ overviewerConfig.js
@@ -0,0 +1,73 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "insertcoin"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1606385498",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "world-upper-right",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "upper-right",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "insertcoin",
+            "last_rendertime": 0,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -208,
+                96,
+                48
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -208,
+                96,
+                48
+            ],
+            "north_direction": 1,
+            "render_in_progress": true,
+            "forcerendertime": 1606385165
+        }
+    ]
+};
